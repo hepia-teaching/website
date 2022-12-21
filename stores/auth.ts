8ab2a3c6d6975987ea07e73c430004310ca82768
CapitaineToinon <antoinesutter@gmail.com>
2022-12-21 23:34:35 +0100

    Fixed useNuxtApp in store

--- stores/auth.ts
+++ stores/auth.ts
@@ -5,7 +5,6 @@ import { loginSchema } from '@/zod/auth'
 import { z } from 'zod'
 
 export const useAuthStore = defineStore('auth', () => {
-	const { $trpc } = useNuxtApp()
 	const user = ref<User | null>(null)
 	const loggedIn = computed(() => user.value !== null)
 
@@ -16,10 +15,12 @@ export const useAuthStore = defineStore('auth', () => {
 	}
 
 	async function login(values: z.infer<typeof loginSchema>) {
+		const { $trpc } = useNuxtApp()
 		user.value = await $trpc.auth.login.mutate(values)
 	}
 
 	async function logout() {
+		const { $trpc } = useNuxtApp()
 		await $trpc.auth.logout.mutate()
 		user.value = null
 	}
