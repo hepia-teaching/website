07ff4a6cef067f230b5b55170463407ce935fea1
CapitaineToinon <antoinesutter@gmail.com>
2022-12-21 18:40:09 +0100

    Fixed playwright test

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
 	<div class="mx-auto mt-5 flex h-full w-96 flex-col gap-3">
-		<FancyTitle>Welcome to Hepia</FancyTitle>
+		<FancyTitle data-testid="homepage-title">Welcome to Hepia</FancyTitle>
 	</div>
 </template>
