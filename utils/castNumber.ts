c2fa6b111cef0092b7b836c79784f08d7d313c0d
CapitaineToinon <antoinesutter@gmail.com>
2022-12-22 09:38:08 +0100

    Fixed cast number

--- utils/castNumber.ts
+++ utils/castNumber.ts
@@ -1,5 +1,8 @@
 import { FormKitNode } from '@formkit/core'
 
 export function castNumber(node: FormKitNode) {
-	node.hook.input((value, next) => next(Number(value)))
+	node.hook.input((value, next) => {
+		const casted = Number(value)
+		return next(!isNaN(casted) ? casted : 0)
+	})
 }
