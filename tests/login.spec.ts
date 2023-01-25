30c4091fcd62adf25952be07a6975a0e34e5120e
CapitaineToinon <antoinesutter@gmail.com>
2023-01-25 22:18:26 +0100

    Adding login test (#28)
    * Adding login test
    
    * Fixed workflow file
    
    * Adding login sepcs
    
    * Adding database URL + testing cache
    
    * Adding ports
    
    * Pushing database

---
+++ tests/login.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test'
+import { PrismaClient, User } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+
+const users: Pick<User, 'email' | 'role'>[] = [
+	{
+		email: faker.datatype.uuid() + '@hepia.com',
+		role: 'Teacher',
+	},
+	{
+		email: faker.datatype.uuid() + '@hepia.com',
+		role: 'Student',
+	},
+	{
+		email: faker.datatype.uuid() + '@hepia.com',
+		role: 'Admin',
+	},
+]
+
+test.beforeAll(async () => {
+	const prisma = new PrismaClient()
+
+	await prisma.user.createMany({
+		data: users,
+	})
+})
+
+users.map((user) => {
+	test(`login as ${user.role}`, async ({ page }) => {
+		await page.goto('http://localhost:3000/')
+		await expect(page.getByTestId('homepage-title')).toHaveText('Login')
+
+		const responsePromise = page.waitForResponse(
+			'http://localhost:3000/api/trpc/auth.login?batch=1'
+		)
+
+		const element = page.getByTestId('login-email')
+		await element.selectText()
+		await element.type(user.email)
+		await page.getByText('submit').click()
+		const response = await responsePromise
+
+		expect(response.request().method()).toBe('POST')
+		expect(response.status()).toBe(200)
+		expect(page.url()).toBe('http://localhost:3000/')
+		expect(await page.getByTestId('navbar-role').textContent()).toBe(user.role)
+	})
+})
