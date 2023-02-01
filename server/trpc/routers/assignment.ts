b9e19a38435e6cdb4faa0a44c2d01af4725f351b
CapitaineToinon <antoinesutter@gmail.com>
2023-02-01 18:08:21 +0100

    Fixed delete

--- server/trpc/routers/assignment.ts
+++ server/trpc/routers/assignment.ts
@@ -7,10 +7,11 @@ import {
 } from '@/zod/assignment'
 import { TRPCError } from '@trpc/server'
 import { subject } from '@casl/ability'
+import { Prisma } from '@prisma/client'
 
 export const assignmentRouter = router({
 	myAssignments: protectedProcedure.query(async ({ ctx }) => {
-		const assignments = await ctx.prisma.assignements.findMany({
+		return await ctx.prisma.assignements.findMany({
 			where: {
 				course: {
 					learning: {
@@ -27,88 +28,97 @@ export const assignmentRouter = router({
 				course: true,
 			},
 		})
-
-		return assignments
 	}),
-	create: protectedProcedure.input(createSchema).mutation(({ input, ctx }) => {
-		if (ctx.ability.cannot('create', 'Assignement')) {
-			throw new TRPCError({
-				code: 'FORBIDDEN',
-			})
-		}
-
-		return ctx.prisma.assignements.create({
-			data: {
-				endDate: input.endDate,
-				startDate: input.startDate,
-				estimated_time: input.estimate_time,
-				description: input.description,
-				roomId: input.course.roomId,
-				fieldId: input.course.fieldId,
-				season: input.course.semester.season,
-				year: input.course.semester.year,
-			},
-		})
-	}),
-	update: protectedProcedure.input(updateSchema).mutation(({ input, ctx }) => {
-		if (ctx.ability.cannot('update', 'Assignement')) {
-			throw new TRPCError({
-				code: 'FORBIDDEN',
-			})
-		}
-
-		return ctx.prisma.assignements.update({
-			where: {
-				id_roomId_fieldId_year_season: {
-					id: input.id,
-					roomId: input.roomId,
-					fieldId: input.fieldId,
-					season: input.season,
-					year: input.year,
-				},
-			},
-			data: {
-				endDate: input.endDate,
-				startDate: input.startDate,
-				estimated_time: input.estimated_time,
-				description: input.description,
-				roomId: input.roomId,
-				fieldId: input.fieldId,
-				season: input.season,
-				year: input.year,
-			},
-		})
-	}),
-	delete: protectedProcedure
-		.input(deleteSchemaAssignement)
+	create: protectedProcedure
+		.input(createSchema)
 		.mutation(async ({ input, ctx }) => {
-			let res = await ctx.prisma.assignements.delete({
-				where: {
-					id_roomId_fieldId_year_season: {
-						id: input.id,
-						roomId: input.course.roomId,
-						fieldId: input.course.fieldId,
-						season: input.course.semester.season,
-						year: input.course.semester.year,
-					},
-				},
-				include: {
-					course: true,
-				},
-			})
-
-			if (!res) {
-				throw new TRPCError({
-					code: 'NOT_FOUND',
-				})
-			}
-			if (ctx.ability.cannot('delete', subject('Assignement', res))) {
+			if (ctx.ability.cannot('create', 'Assignement')) {
 				throw new TRPCError({
 					code: 'FORBIDDEN',
 				})
 			}
 
-			return res
+			return await ctx.prisma.assignements.create({
+				data: {
+					endDate: input.endDate,
+					startDate: input.startDate,
+					estimated_time: input.estimate_time,
+					description: input.description,
+					roomId: input.course.roomId,
+					fieldId: input.course.fieldId,
+					season: input.course.semester.season,
+					year: input.course.semester.year,
+				},
+			})
+		}),
+	update: protectedProcedure
+		.input(updateSchema)
+		.mutation(async ({ input, ctx }) => {
+			if (ctx.ability.cannot('update', 'Assignement')) {
+				throw new TRPCError({
+					code: 'FORBIDDEN',
+				})
+			}
+
+			return await ctx.prisma.assignements.update({
+				where: {
+					id_roomId_fieldId_year_season: {
+						id: input.id,
+						roomId: input.roomId,
+						fieldId: input.fieldId,
+						season: input.season,
+						year: input.year,
+					},
+				},
+				data: {
+					endDate: input.endDate,
+					startDate: input.startDate,
+					estimated_time: input.estimated_time,
+					description: input.description,
+					roomId: input.roomId,
+					fieldId: input.fieldId,
+					season: input.season,
+					year: input.year,
+				},
+			})
+		}),
+	delete: protectedProcedure
+		.input(deleteSchemaAssignement)
+		.mutation(async ({ input, ctx }) => {
+			try {
+				let res = await ctx.prisma.assignements.delete({
+					where: {
+						id_roomId_fieldId_year_season: {
+							id: input.id,
+							roomId: input.roomId,
+							fieldId: input.fieldId,
+							season: input.season,
+							year: input.year,
+						},
+					},
+					include: {
+						course: true,
+					},
+				})
+
+				if (ctx.ability.cannot('delete', subject('Assignement', res))) {
+					throw new TRPCError({
+						code: 'FORBIDDEN',
+					})
+				}
+
+				return res
+			} catch (e) {
+				if (e instanceof Prisma.PrismaClientKnownRequestError) {
+					if (e.code === 'P2025') {
+						throw new TRPCError({
+							code: 'NOT_FOUND',
+						})
+					}
+				}
+
+				throw e
+			}
 		}),
 	get: protectedProcedure.input(getSchema).query(async ({ input, ctx }) => {
 		const assignement = await ctx.prisma.assignements.findUnique({
