aa8d1608e714591f80ebff880c6e58c3b7c84ef3
CapitaineToinon <antoinesutter@gmail.com>
2023-03-05 22:31:45 +0100

    prettier

--- components/AssignmentLoadPreview.vue
+++ components/AssignmentLoadPreview.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { propsToAttrMap } from '@vue/shared';
+import { propsToAttrMap } from '@vue/shared'
 import dayjs from 'dayjs'
-import isBetween from 'dayjs/plugin/isBetween';
+import isBetween from 'dayjs/plugin/isBetween'
 const { $trpc } = useNuxtApp()
 dayjs.extend(isBetween)
 
@@ -9,32 +9,43 @@ type Assignment = Awaited<
 	ReturnType<typeof $trpc.assignment.myAssignments.query>
 >[0]
 
-const props = defineProps<{ newAssignment: Assignment, others: Assignment[] }>()
+const props = defineProps<{ newAssignment: Assignment; others: Assignment[] }>()
 let startDate = dayjs(props.newAssignment.startDate)
 let endDate = dayjs(props.newAssignment.endDate)
-let nbDays = endDate.diff(startDate, "day")
+let nbDays = endDate.diff(startDate, 'day')
 
 let workloadPerDay = Array.from({ length: nbDays }).map((_, key) => {
 	return props.others.reduce((acc, assignment) => {
-		if (startDate.add(key, "day").isBetween(assignment.startDate, assignment.endDate)) {
-			let assignmentNbDays = dayjs(assignment.endDate).diff(assignment.startDate, "day")
-			let assignmentWorkloadPerDay = assignment.estimated_time / assignmentNbDays
+		if (
+			startDate
+				.add(key, 'day')
+				.isBetween(assignment.startDate, assignment.endDate)
+		) {
+			let assignmentNbDays = dayjs(assignment.endDate).diff(
+				assignment.startDate,
+				'day'
+			)
+			let assignmentWorkloadPerDay =
+				assignment.estimated_time / assignmentNbDays
 
 			acc += assignmentWorkloadPerDay
 		}
 
 		return acc
 	}, 0)
-});
-
+})
 </script>
 
 <template>
-	<div class="flex gap-3 items-stretch justify-between w-full">
+	<div class="flex w-full items-stretch justify-between gap-3">
 		<span v-for="(hours, key) in workloadPerDay">
-			<div class="text-center p-3 gap-2 grid flex-grow card bg-base-300 rounded-box place-items-center">
+			<div
+				class="card rounded-box grid flex-grow place-items-center gap-2 bg-base-300 p-3 text-center"
+			>
 				<h1 class="text-3xl font-bold">{{ hours.toFixed(2) }}h</h1>
-				<p class="font-light text-gray-400">{{ startDate.add(key+1, "day").format("dddd, DD MMMM YYYY") }}</p>
+				<p class="font-light text-gray-400">
+					{{ startDate.add(key + 1, 'day').format('dddd, DD MMMM YYYY') }}
+				</p>
 			</div>
 			<div class="divider divider-horizontal"></div>
 		</span>
