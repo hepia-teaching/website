330016c062daca71a2e21d9d67cbd9637263e7aa
julien.seemulle <julien.seemuller@etu.hesge.ch>
2023-03-16 13:06:06 +0100

    Toasts on courses

--- pages/courses/create.vue
+++ pages/courses/create.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { z } from 'zod'
 import { createSchema } from '@/zod/course'
+import toast from '~~/plugins/toast';
 
 const { $trpc } = useNuxtApp()
+const toasts = useToastStore()
 
 const { ZodForm, ZodKit, reset } = useZodFormKit({
 	schema: createSchema,
@@ -31,47 +33,28 @@ const semestersOptions = semesters.value?.map((semester) => ({
 }))
 
 async function submit(values: z.infer<typeof createSchema>) {
-	await $trpc.course.create.mutate(values)
-	reset()
+	try {
+		await $trpc.course.create.mutate(values)
+		reset()
+		toasts.success("Successfully added course.")
+	}
+	catch (e) {
+		toasts.error(e)
+	}
 }
 </script>
 
 <template>
 	<div class="flex flex-col gap-3">
 		<FancyTitle>Create a course</FancyTitle>
-		<ZodForm
-			v-if="$can('create', 'Course')"
-			@submit="submit"
-		>
-			<ZodKit
-				label="Room"
-				name="roomId"
-				type="select"
-				data-testid="room"
-				:options="roomsOptions"
-			/>
+		<ZodForm v-if="$can('create', 'Course')" @submit="submit">
+			<ZodKit label="Room" name="roomId" type="select" data-testid="room" :options="roomsOptions" />
 
-			<ZodKit
-				label="Field"
-				name="fieldId"
-				type="select"
-				data-testid="field"
-				:options="fieldsOptions"
-			/>
+			<ZodKit label="Field" name="fieldId" type="select" data-testid="field" :options="fieldsOptions" />
 
-			<ZodKit
-				label="Semester"
-				name="semester"
-				type="select"
-				data-testid="semester"
-				:options="semestersOptions"
-			/>
+			<ZodKit label="Semester" name="semester" type="select" data-testid="semester" :options="semestersOptions" />
 
-			<ZodKit
-				label="Description"
-				name="description"
-				type="textarea"
-			/>
+			<ZodKit label="Description" name="description" type="textarea" />
 		</ZodForm>
 	</div>
 </template>
