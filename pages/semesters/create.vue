e3e068cbf6bf5cbda672f0826f7b48e3a0eb39e2
julien.seemulle <julien.seemuller@etu.hesge.ch>
2023-03-16 13:13:38 +0100

    Toasts on semesters

--- pages/semesters/create.vue
+++ pages/semesters/create.vue
@@ -9,10 +9,17 @@ const { ZodForm, ZodKit, reset } = useZodFormKit({
 })
 
 const { $trpc } = useNuxtApp()
+const toasts = useToastStore()
 
 async function submit(values: z.infer<typeof createSchema>) {
-	await $trpc.semester.create.mutate(values)
-	reset()
+	try {
+		await $trpc.semester.create.mutate(values)
+		reset()
+		toasts.success("Successfully created semester.")
+	}
+	catch(e){
+		toasts.error(e)
+	}
 }
 </script>
 
@@ -20,17 +27,8 @@ async function submit(values: z.infer<typeof createSchema>) {
 	<div class="flex flex-col gap-3">
 		<FancyTitle>Create semester</FancyTitle>
 		<ZodForm @submit="submit">
-			<ZodKit
-				label="Year"
-				type="number"
-				name="year"
-			/>
-			<ZodKit
-				label="Season"
-				type="select"
-				name="season"
-				:options="Season"
-			/>
+			<ZodKit label="Year" type="number" name="year" />
+			<ZodKit label="Season" type="select" name="season" :options="Season" />
 		</ZodForm>
 	</div>
 </template>
