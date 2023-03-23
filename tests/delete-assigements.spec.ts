9acc192d72e3e959d2bac46e239c56a753b3b7d3
CapitaineToinon <antoinesutter@gmail.com>
2023-03-23 23:19:09 +0100

    Trying to fix playwright

--- tests/delete-assigements.spec.ts
+++ tests/delete-assigements.spec.ts
@@ -1,33 +1,44 @@
 import { test, expect } from '@playwright/test'
-import { PrismaClient, User, Field, Room, Semester } from '@prisma/client'
+import {
+	PrismaClient,
+	User,
+	Field,
+	Room,
+	Semester,
+	Course,
+	Assignements,
+} from '@prisma/client'
 import { faker } from '@faker-js/faker'
-import { createCookie } from './utils/cookie'
+import { Cookie, createCookie } from './utils/cookie'
 import dayjs from 'dayjs'
 
-const teacher: Pick<User, 'email' | 'role'> = {
-	email: faker.datatype.uuid() + '@hepia.com',
-	role: 'Teacher',
-}
+let cookie: Cookie | null = null
+let createdAssignement: Assignements | null = null
 
-const field: Pick<Field, 'name'> = {
-	name: faker.datatype.uuid(),
-}
-
-const room: Pick<Room, 'number'> = {
-	number: faker.datatype.uuid(),
-}
-
-const semester: Semester = {
-	name: faker.lorem.word(),
-	year: faker.datatype.number({ min: 2000, max: 3000 }),
-	season: 'Automn',
-}
-
-const courseDescription = faker.datatype.uuid()
-const newCourseDescription = faker.datatype.uuid()
-
-test(`Delete an assignement as teacher`, async ({ browser }) => {
+test.beforeEach(async () => {
 	const prisma = new PrismaClient()
+
+	const teacher: Pick<User, 'email' | 'role'> = {
+		email: faker.datatype.uuid() + '@hepia.com',
+		role: 'Teacher',
+	}
+
+	const field: Pick<Field, 'name'> = {
+		name: faker.datatype.uuid(),
+	}
+
+	const room: Pick<Room, 'number'> = {
+		number: faker.datatype.uuid(),
+	}
+
+	const semester: Semester = {
+		name: faker.lorem.word(),
+		year: faker.datatype.number({ min: 2000, max: 3000 }),
+		season: 'Automn',
+	}
+
+	const courseDescription = faker.datatype.uuid()
+	const newCourseDescription = faker.datatype.uuid()
 
 	const createdField = await prisma.field.create({
 		data: field,
@@ -78,27 +89,40 @@ test(`Delete an assignement as teacher`, async ({ browser }) => {
 		],
 	})
 
-	const { id, fieldId, roomId, season, year } =
-		await prisma.assignements.create({
-			data: {
-				fieldId: createdCourse.fieldId,
-				roomId: createdCourse.roomId,
-				season: createdCourse.season,
-				year: createdCourse.year,
-				description: newCourseDescription,
-				estimated_time: faker.datatype.number({ min: 1, max: 100 }),
-				startDate: dayjs().subtract(1, 'day').toDate(),
-			},
-		})
+	createdAssignement = await prisma.assignements.create({
+		data: {
+			fieldId: createdCourse.fieldId,
+			roomId: createdCourse.roomId,
+			season: createdCourse.season,
+			year: createdCourse.year,
+			description: newCourseDescription,
+			estimated_time: faker.datatype.number({ min: 1, max: 100 }),
+			startDate: dayjs().subtract(1, 'day').toDate(),
+		},
+	})
 
-	const cookie = await createCookie(createdTeacher)
+	cookie = await createCookie(createdTeacher)
+})
+
+test(`Delete an assignement as teacher`, async ({ browser }) => {
+	if (!cookie) {
+		throw new Error('Cookie is null')
+	}
+
+	if (!createdAssignement) {
+		throw new Error('Assignement is null')
+	}
+
 	const browserContext = await browser.newContext()
 	await browserContext.addCookies([cookie])
 	const page = await browserContext.newPage()
 
-	const path = [id, fieldId, roomId, season, year].join('-')
-	let res = await page.goto(`http://localhost:3000/assignements/delete/${path}`)
+	const { id, fieldId, roomId, season, year } = createdAssignement
+	let res = await page.goto(
+		`http://localhost:3000/courses/${fieldId}-${roomId}-${season}-${year}/assignements/${id}/delete`
+	)
 	expect(res?.status()).toBe(200)
+
 	await expect(page.getByTestId('homepage-title')).toHaveText(
 		'Please confirm the deletion'
 	)
