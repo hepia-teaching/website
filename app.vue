aeab173e0f805bde2b2aefea8aa87c6a4ed73050
CapitaineToinon <antoinesutter@gmail.com>
2023-03-28 20:20:08 +0200

    Fixed toast position

--- app.vue
+++ app.vue
@@ -1,14 +1,14 @@
 <template>
 	<div
 		data-theme="light"
-		class="relative min-h-screen bg-base-200"
+		class="relative min-h-[200vh] bg-base-200"
 	>
 		<Navbar />
 		<NuxtLayout>
 			<NuxtLoadingIndicator />
 			<NuxtPage />
 		</NuxtLayout>
-		<div class="absolute right-5 bottom-5 w-[34rem]">
+		<div class="fixed right-5 bottom-5 w-[34rem]">
 			<Toasts />
 		</div>
 	</div>
